c983d26770fd279dd983b58e11c8539e3fb89e29
Mark Lech <39778464+mlech413@users.noreply.github.com>
2018-07-18 18:37:43 -0400

    final commit

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -27,7 +27,9 @@ function start() {
     var query = "SELECT * FROM products ORDER BY department_name, product_name;";
     connection.query(query, function(err, res) {
         for (var i = 0; i < res.length; i++){
+            //Loop through the query results, but ONLY DISPLAY PRODUCTS THAT ARE IN STOCK
             if (res[i].stock_quantity > 0){
+                //Populate and format the product list using npm cli-table
                 table.push([res[i].item_id, res[i].product_name, res[i].department_name, "$" + res[i].price]);
             }
         };
@@ -39,14 +41,16 @@ function start() {
 
 };
 
-    
+//products() dsplays the full product list and then asks user to pick a product for purchase
 function products(res){
 
     console.log(" ");
-          
+
+    //Display the product list in the cli-table      
     console.log(table.toString());
 
     console.log(" ");
+    //Prompt user to select a product to buy
     inquirer.prompt(
         {
             name: "purchaseID",
@@ -63,6 +67,7 @@ function products(res){
 
         var itemFound = false;
         for (var i = 0; i < res.length; i++) {
+            //Populate variables with the product that the user selected
             if (answer1.purchaseID == res[i].item_id) {
                 itemFound = true;
                 selectedItemID = res[i].item_id;
@@ -73,10 +78,12 @@ function products(res){
              }
         }
         if (!itemFound) {
+            //No item was returned, so display message that user entry was invalid
             console.log("\n INVALID ENTRY:".yellow + " Please enter the ID number of a product.".cyan)
             products(res);
         }
         else {
+            //User selected and item and it was returned by the query
             quantity(res)
 
         };
@@ -86,7 +93,7 @@ function products(res){
 };
 
 
-
+//Asks user for the quantity of the product that they would like to purchase
 function quantity(res){
     console.log(" ");
     inquirer.prompt(
@@ -98,39 +105,43 @@ function quantity(res){
     )
     .then(function(answer2) {
         if (answer2.purchaseQty == 0) {
+            //User selected 0 products to purchase, so exit this question and go back up to the products selection
             products(res);
         }
         else if (answer2.purchaseQty < 1 || answer2.purchaseQty < '1' || answer2.purchaseQty > 999 || answer2.purchaseQty > '999') {
+            //User made invalid selections, so display a message and ask for the quantity again
             console.log("\nINVALID ENTRY!".yellow)
             quantity(res);
         }
         else if (selectedStockQty == 0) {
+            //The item selected is out of stock (out of stock items were already not displayed, but this is a safeguard)
             console.log("\nSORRY, THAT ITEM IS CURRENTLY OUT OF STOCK!".red);
             products(res);
         }
         else if (answer2.purchaseQty > selectedStockQty) {
+            //User selected a quantity that is greater than what is in stock, so display qhat is in stock, and ask quantity again
             console.log("\nSORRY, INSUFFICIENT QUANTITY! ".red + selectedStockQty + " in stock. Please enter a lower amount:");
             quantity(res);
         }
         else {
+            //User entered valid quantity, proceed to complete the purchase
             var selectedPurchaseQty = answer2.purchaseQty;
             makePurchase(selectedStockQty, selectedPurchaseQty);
         };
     });
   };
 
-
+  //Completes the purchase by updating total and displaying amount purchased and total cost
   function makePurchase(selectedStockQty, selectedPurchaseQty) {
+    //deduct number that user purchased and update table with the new quantity
     var query = "UPDATE products SET stock_quantity = (stock_quantity - " + selectedPurchaseQty + ") WHERE item_id = " + selectedItemID + ";";
     connection.query(query, function(err, res) {
         if (err) throw err;
+        //display messages for the purchase
         console.log("\nCongratulations!".yellow)
-        if (selectedPurchaseQty == 1) {
-            console.log("You purchased 1 '" + selectedProdName + "'. " + (selectedStockQty - selectedPurchaseQty) + " remaining in stock.");
-        }
-        else {
-            console.log("You purchased " + selectedPurchaseQty + " of '" + selectedProdName + "'. " + (selectedStockQty - selectedPurchaseQty) + " remaining in stock.");
-        }
+        //Show user how many they purchased, and how many remain in stock
+        console.log("You purchased " + selectedPurchaseQty + " of '" + selectedProdName + "'. " + (selectedStockQty - selectedPurchaseQty) + " remaining in stock.");
+        //Show the user their total cost for the purchase, and exit the transaction
         console.log(("Your total cost is $" + selectedPurchaseQty + selectedPrice).yellow)
         connection.end();
     });
